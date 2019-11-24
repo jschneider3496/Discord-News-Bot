dea39afcb8741f22eeaad430a4d8f084fd3d29bf
Johannes Schneider <jojo20872@gmail.com>
2019-11-24 17:54:38 -0500

    Sort !news commands

--- index.js
+++ index.js
@@ -37,72 +37,104 @@ bot.on('message', msg => {
         case 'repeat':
             msg.channel.sendMessage(search);
             break;
-        /* Lists top-headlines from keywords */
-        case 'news':
-            /* Uses newsapi for news data */
-
-            /* Determine the count (if there is one) */
-            var count; /* How many titles to show */
-            var firstWord; /* Index of first search keyword */
-
-            if (!args[1]) {
-                msg.channel.sendMessage("Please indicate keywords.");
-                return;
-            } else if (isNaN(args[1])) { /* Default: No arguments or no number, show 3 */
-                count = 3;
-                firstWord = 1;
-            } else if (!args[2]) {
-                msg.channel.sendMessage("Please indicate keywords.");
-                return;
-            } else if (parseInt(args[1]) > 10) { /* Case: Over 10 (takes too long) */
-                msg.channel.sendMessage("Max number of articles I can fetch is 10.");
-                count = 10;
-                firstWord = 2;
-            } else {
-                count = parseInt(args[1]);
-                firstWord = 2;
-            }
-
-            /* Get last week for most recent headlines */
-            var lastWeek = getLastWeek();
-
-            /* Uses newsapi for news data */
-            var url = 'https://newsapi.org/v2/everything?' +
-                'apiKey=' + auth.newsapi +
-                "&from=" + lastWeek +
-                "&sortBy=popularity";
-
-            /* Append search keywords */
-            for (var i = firstWord; i < args.length; i++) {
-                url = url.concat("&q=", args[i]);
-            }
-
-            /* Returns JSON object with news data that can be iterated over. */
-            axios(url)
-                .then(function (response) {
-                    /* JSON data containing article information */
-                    var jsonData = response.data.articles;
-
-                    if (jsonData.length < count) {
-                        msg.channel.send("Too many requested, sending " + jsonData.length + " instead.")
-                        count = jsonData.length;
-                    }
-
-                    /* Print in chat titles of articles */
-                    for (var i = 0; i < count; i++) {
-                        const newsEmbed = new Discord.RichEmbed()
-                            .setColor('#4DF4F6')
-                            .setTitle("Article " + (i + 1) + ": " + jsonData[i].title)
-                            .setURL(jsonData[i].url)
-                            .addField('Description', jsonData[i].description)
-                            .addField('Published', jsonData[i].publishedAt)
-                            .setImage(jsonData[i].urlToImage);
-                        msg.channel.send(newsEmbed);
-                    }
-                }).catch((err) => {
-                    console.log(err);
-                });
+        case 'news':                    /* Show popular news */       
+        case 'newspop':                 /* Show popular news */  
+            getNews(args, "pop");
             break;
+        case 'newspub':                 /* Show recently published news */  
+            getNews(args, "pub");
+            break;
+        case 'newsrel':                 /* Show relevant news */  
+            getNews(args, "rel");
+            break;
+        case 'newsth':                  /* Show top headlines */  
+            getNews(args, "th");
+            break;
+    }
+
+    function getNews(args, sortBy) {
+        /* Uses newsapi for news data */
+
+        /* Determine the count (if there is one) */
+        var count; /* How many titles to show */
+        var firstWord; /* Index of first search keyword */
+
+        if (!args[1]) {
+            msg.channel.sendMessage("Please indicate keywords.");
+            return;
+        } else if (isNaN(args[1])) { /* Default: No arguments or no number, show 3 */
+            count = 3;
+            firstWord = 1;
+        } else if (!args[2]) {
+            msg.channel.sendMessage("Please indicate keywords.");
+            return;
+        } else if (parseInt(args[1]) > 10) { /* Case: Over 10 (takes too long) */
+            msg.channel.sendMessage("Max number of articles I can fetch is 10.");
+            count = 10;
+            firstWord = 2;
+        } else {
+            count = parseInt(args[1]);
+            firstWord = 2;
+        }
+
+        var url;
+        var lastWeek = getLastWeek(); /* Get last week for most recent headlines */
+
+        switch (sortBy) {                                   /* How news will be sorted */
+            case 'pop':                                     /* Popularity */
+                url = 'https://newsapi.org/v2/everything?' +
+                    'apiKey=' + auth.newsapi +
+                    "&from=" + lastWeek +
+                    "&sortBy=popularity";
+                break;
+            case 'pub':                                     /* Publish date */
+                url = 'https://newsapi.org/v2/everything?' +
+                    'apiKey=' + auth.newsapi +
+                    "&from=" + lastWeek +
+                    "&sortBy=publishedAt";
+                break;
+            case 'rel':                                     /* Relevance */
+                url = 'https://newsapi.org/v2/everything?' +
+                    'apiKey=' + auth.newsapi +
+                    "&from=" + lastWeek +
+                    "&sortBy=relevancy";
+                break;
+            case 'th':                                      /* Top headline */
+                url = 'https://newsapi.org/v2/top-headlines?country=us' +
+                    '&apiKey=' + auth.newsapi;
+                break;
+        }
+
+        /* Append search keywords */
+        for (var i = firstWord; i < args.length; i++)
+            url = url.concat("&q=", args[i]);
+
+        msg.channel.send(url);
+        /* Returns JSON object with news data that can be iterated over. */
+        axios(url)
+            .then(function (response) {
+                /* JSON data containing article information */
+                var jsonData = response.data.articles;
+
+                if (jsonData.length < count) {
+                    msg.channel.send("Not that many articles, sending " + jsonData.length + " instead.")
+                    count = jsonData.length;
+                }
+
+                /* Print in chat titles of articles */
+                for (var i = 0; i < count; i++) {
+                    const newsEmbed = new Discord.RichEmbed()
+                        .setColor('#4DF4F6')
+                        .setTitle("Article " + (i + 1) + ": " + jsonData[i].title)
+                        .setURL(jsonData[i].url)
+                        .addField('Description', jsonData[i].description)
+                        .addField('Published', jsonData[i].publishedAt)
+                        .setImage(jsonData[i].urlToImage);
+                    msg.channel.send(newsEmbed);
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
     }
 });
 
@@ -113,8 +145,6 @@ function getLastWeek() {
     var lastWeekMonth = lastWeek.getMonth() + 1;
     var lastWeekDay = lastWeek.getDate();
     var lastWeekYear = lastWeek.getFullYear();
-
-    var lastWeekDisplay = lastWeekMonth + "/" + lastWeekDay + "/" + lastWeekYear;
     var lastWeekDisplayPadded = ("00" + lastWeekMonth.toString()).slice(-2) + "/"
         + ("00" + lastWeekDay.toString()).slice(-2) + "/" + ("0000" + lastWeekYear.toString()).slice(-4);
     return lastWeekDisplayPadded;
