78d81ce8de4d41c174f9067070fde71a40739948
Dan <bolt12326@gmail.com>
2019-11-24 14:45:12 -0500

    add search key

--- index.js
+++ index.js
@@ -40,24 +40,42 @@ bot.on('message', msg => {
         /* Lists args[1] number of top-headlines from U.S. */
         case 'news':
             /* Uses newsapi for news data */
+             // Determine the count (if there is one)
+             var count; /* How many titles to show */
+             var firstWord;
+             if(!args[1] || isNaN(args[1])) {
+             	count = 5;
+              firstWord = 1;
+             }     
+            else if (parseInt(args[1]) > 10) { /* Case: Over 10 (takes too long) */
+              msg.channel.sendMessage("Max number of articles I can fetch is 10.");
+              count = 10;
+              firstWord = 2;
+            } else {
+            count = parseInt(args[1]);
+            firstWord = 2;
+            }
+            	
+      
+            /* Uses newsapi for news data */
             var url = 'https://newsapi.org/v2/top-headlines?' +
                 'country=us&' +
                 'apiKey=' + auth.newsapi;
+                
+            for(var i = firstWord; i < args.length; i++) {
+               url = url.concat("&q=", args[i]); 
+            }    
             /* Returns JSON object with news data that can be iterated over. */
             /* Currently shows titles */
             axios(url)
                 .then(function (response) {
-                    var count; /* How many titles to show */
-                    if (!args[1]) /* Default: no args[1] */
-                        count = 5;
-                    else if (parseInt(args[1]) > 10) { /* Case: Over 10 (takes too long) */
-                        msg.channel.sendMessage("Max number of articles I can fetch is 10.");
-                        count = 10;
-                    } else
-                        count = parseInt(args[1]);
+
 
                     /* JSON data containing article information */
                     var jsonData = response.data.articles;
+
+                    if(jsonData.length < count)
+                    	count = jsonData.length;
 
                     /* Print in chat titles of articles */
                     for (var i = 1; i < count + 1; i++) {
